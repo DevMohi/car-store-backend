f8f872b666528d1e087ed4d80619c1a6582c534d
DevMohi <morphex72@gmail.com>
2025-04-12 17:18:43 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,15 @@ const app: Application = express();
 //parser
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://car-store-assingment.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 
 //application routes
 app.use('/api/v1', router);
